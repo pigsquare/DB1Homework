90db5ee5f877cba70d5377fbf1a20a3b307899f9
zhufz <zhufz3528ui@126.com>
2021-01-11 21:12:59 +0800

    feat: re-init

---
+++ src/main/java/com/zfz/coursemanage/entity/SC.java
@@ -0,0 +1,10 @@
+package com.zfz.coursemanage.entity;
+
+import lombok.Data;
+@Data
+public class SC {
+    private String sno;
+    private String cno;
+    private double grade;
+    private double point;
+}
